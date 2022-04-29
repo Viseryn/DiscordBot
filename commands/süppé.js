bb29af8e22fc2ebb3f5ce56cf576ab0e98e756f5
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-04-29 14:55:39 +0200

    Documentation + süppé command

--- commands/süppé.js
+++ commands/süppé.js
@@ -1,10 +1,81 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
+
+/**
+ * chooseRandomWithWeights
+ * @param {*} list A weighted list of the format [{ value: ..., probability: ... }, ...]
+ * @returns The value of an element of the list and if the reply is private.
+ */
+const chooseRandomWithWeights = (list) => {
+    let rand = Math.random(); // Random number in [0, 1)
+    let i, sumWeights = 0, threshold = 0, pickedValue;
+    
+    // Check whether the weights in the list add up to 1
+    for (i = 0; i < list.length; i++) {
+        sumWeights += list[i].probability;
+    }
+    
+    sumWeights = Math.ceil(sumWeights);
+
+    if (sumWeights > 1) {
+        console.log('>>> WARNING: Weights sum up to a value greater than 1.');
+    } else if (sumWeights < 1) {
+        console.log('>>> WARNING: Weights sum up to a value lesser than 1.');
+        return [
+            list[0].value 
+                + ' Und sag Yusel, er soll den Bot mal vernünftig konfigurieren.',
+            false
+        ];
+    }
+
+    // Choose random element from list
+    for (i = 0; i < list.length; i++) {
+        threshold += list[i].probability;
+
+        if (rand < threshold) {
+            pickedValue = list[i].value;
+            break;
+        }
+    }
+
+    return [pickedValue, false];
+};
+
+
+/**
+ * 
+ */
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('süppé')
-        .setDescription('Süppé'),
+        .setDescription('Gelüstet es dir nach einer warmen Süppé?')
+        .addMentionableOption(option =>
+            option.setName('für')
+                .setDescription('Soll die Süppé etwa für jemand Besonderes sein?!')
+                .setRequired(false)),
     async execute(interaction) {
-        await interaction.reply('Halt die Fresse Denis!');
+        // Fetch options
+        const user = interaction.user;
+        let mention = interaction.options.getMentionable('für');
+        if(mention === null) mention = user;
+
+        // List of Süppé-replies
+        const suppeList = [{ 
+                value: `Hallo ${mention}, es gibt köstliche Süppé!`, 
+                probability: .6 
+            },
+            { 
+                value: `Hallo ${mention}, heute gibt es leider keine Süppé.`, 
+                probability: .3
+            },
+            { 
+                value: `Halt die Fresse, Denis!`, 
+                probability: .1 
+            },
+        ];
+
+        // Choose a random reply
+        let [suppe, isPrivate] = chooseRandomWithWeights(suppeList);
+        await interaction.reply({ content: `${suppe}`, ephemeral: isPrivate });
     },
 };
--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-// Require necessary discord.js classes and create new client instance
+/**
+ * STORYWRITER DISCORD BOT
+ * 
+ * @version 1.0.0
+ * @author Viseryn
+ */
+
+
+
+/**
+ * Require necessary (discord.js-)classes and create new client instance
+ */
+
 const fs = require('node:fs');
 const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
-
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 
-// Fetch commands
+/**
+ * Fetch commands
+ */
+
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
@@ -15,21 +29,29 @@ for (const file of commandFiles) {
 }
 
 
-// Log
+/**
+ * Set online status 
+ */
+
 client.once('ready', () => {
-    client.user.setStatus('invisible');
+    client.user.setStatus('dnd');
+    // client.user.setActivity('Test', { type: 'WATCHING' });
+    client.user.setActivity('Wird gerade richtig durchgetestet!');
     console.log('Storywriter: Running.');
 });
 
 
-// Reply to commands
+/**
+ * Reply to commands
+ */
+
 client.on('interactionCreate', async interaction => {
+    // Check whether interaction is a command
 	if (!interaction.isCommand()) return;
-
 	const command = client.commands.get(interaction.commandName);
-
     if (!command) return;
 
+    // If the interaction is an actual command, try to execute.
 	try {
         await command.execute(interaction);
     } catch (error) {
@@ -39,5 +61,8 @@ client.on('interactionCreate', async interaction => {
 });
 
 
-// Login to Discord with client's token
+/**
+ * Login to Discord with client's token
+ */
+
 client.login(token);
